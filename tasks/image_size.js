d585214b5e044734ca827015f7c2d10a152b0549
ArmorDarks <git@lavrins.com>
2017-12-21 00:41:29 +0200

    Format the code with Prettier

--- tasks/image_size.js
+++ tasks/image_size.js
@@ -8,52 +8,64 @@
 'use strict';
 
 module.exports = function(grunt) {
-	var path = require('path');
-	var chalk = require('chalk');
-	var sizer = require('image-size');
-	var types = [
-		'.png',
-		'.gif',
-		'.bmp',
-		'.psd',
-		'.jpg',
-		'.tiff',
-		'.webp'
-	];
+  var path = require('path');
+  var chalk = require('chalk');
+  var sizer = require('image-size');
+  var types = ['.png', '.gif', '.bmp', '.psd', '.jpg', '.tiff', '.webp'];
 
-	grunt.registerMultiTask('image_size', 'retrieve image saze information', function() {
-		var options = this.options({
-			configObject : ''
-		});
+  grunt.registerMultiTask(
+    'image_size',
+    'retrieve image saze information',
+    function() {
+      var options = this.options({
+        configObject: ''
+      });
 
-		this.files.forEach(function (file) {
-			if ((!options.configObject || !options.configObject.length) && !file.dest) {
-				grunt.fail.warn('Neither options.configObject nor dest specified for this task.');
-				return;
-			}
+      this.files.forEach(function(file) {
+        if (
+          (!options.configObject || !options.configObject.length) &&
+          !file.dest
+        ) {
+          grunt.fail.warn(
+            'Neither options.configObject nor dest specified for this task.'
+          );
+          return;
+        }
 
-			var output = [];
-			file.src.forEach(function (src) {
-				if (grunt.file.exists(src) && grunt.file.isFile(src) && types.indexOf(path.extname(src).toLowerCase()) !== -1) {
-					var dimensions = sizer(src);
-					output.push({
-						name : src,
-						width : dimensions.width,
-						height : dimensions.height
-					});
+        var output = [];
+        file.src.forEach(function(src) {
+          if (
+            grunt.file.exists(src) &&
+            grunt.file.isFile(src) &&
+            types.indexOf(path.extname(src).toLowerCase()) !== -1
+          ) {
+            var dimensions = sizer(src);
+            output.push({
+              name: src,
+              width: dimensions.width,
+              height: dimensions.height
+            });
 
-					grunt.verbose.writeln('dimensions of ' + src + ' width: ' + dimensions.width.toString() + ', height: ' + dimensions.height.toString());
-				}
-			});
+            grunt.verbose.writeln(
+              'dimensions of ' +
+                src +
+                ' width: ' +
+                dimensions.width.toString() +
+                ', height: ' +
+                dimensions.height.toString()
+            );
+          }
+        });
 
-			if (options.configObject && options.configObject.length) {
-				grunt.config.set(options.configObject, output);
-			}
+        if (options.configObject && options.configObject.length) {
+          grunt.config.set(options.configObject, output);
+        }
 
-			if (file.dest) {
-				grunt.file.write(file.dest, JSON.stringify(output));
-				grunt.log.writeln('File ' + chalk.cyan(file.dest) + ' created.');
-			}
-		});
-	});
+        if (file.dest) {
+          grunt.file.write(file.dest, JSON.stringify(output));
+          grunt.log.writeln('File ' + chalk.cyan(file.dest) + ' created.');
+        }
+      });
+    }
+  );
 };
